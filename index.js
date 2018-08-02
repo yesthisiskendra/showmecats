4aee187484bc9c4c096483fa274444fee1ba13cc
Kendra Osburn <kendraosburn@Kendras-MacBook-Pro.local>
2018-08-02 09:34:57 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator')
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/passwords', (req, res) => { 
+
+	const count = 5;
+  	const passwords = Array.from(Array(count).keys()).map(i =>
+    		generatePassword(12, false)
+ 	)	
+	res.json(passwords);
+	console.log(`Sent ${count} passwords`);
+});
+
+app.get('*', (res, req) => {
+	res.sendFile(path.join(__dirname,+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`Password generator listening on ${port}`);
+
